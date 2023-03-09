644df0d1edee4d3b7e6350c4a81d6b9cd2cb8d5f
Sumed Tamang <xumed001@gmail.com>
2023-03-09 12:23:07 -0600

    adding basic functionality

---
+++ app.js
@@ -0,0 +1,22 @@
+// Calulator app
+
+// adding
+function add(a, b) {
+    return a + b
+}
+
+// subtracts
+function subtract(a, b){
+    return a - b
+}
+
+// multiply
+function multiply(a, b){
+    return a * b
+}
+
+// divide
+function divide(a, b){
+    let ans = a / b
+    return Number(ans.toFixed(2))
+}
