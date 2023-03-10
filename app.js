ba85bfc5f7403b5802694f0479a0e77271cb0fa4
Sumed Tamang <xumed001@gmail.com>
2023-03-09 20:03:55 -0600

    built UI, adding some more css

--- app.js
+++ app.js
@@ -1,4 +1,28 @@
 // Calulator app
+const keys = document.querySelector('.calulator__keys')
+const display = document.querySelector('.calculator__display')
+
+keys.addEventListener('click', event => {
+    if (!event.target.closest('button')) return
+    
+    const key = event.target
+    const keyValue = key.textContent
+    const displayValue = display.textContent
+    
+    if (key.classList.contains('number')) {
+        if (displayValue === '0') {
+            display.textContent = keyValue
+        } else {
+            display.textContent = displayValue + keyValue
+        }
+    }
+
+    if (key.classList.contains('operator')){
+        console.log(key)
+    }
+    
+})
+
 
 // adding
 function add(a, b) {
@@ -20,3 +44,5 @@ function divide(a, b){
     let ans = a / b
     return Number(ans.toFixed(2))
 }
+
+
